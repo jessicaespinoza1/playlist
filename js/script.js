c844d3495e037305d11289335fbf7c91a924ac2d
jessicaespinoza1 <13jessica.espinoza@gmail.com>
2020-01-16 02:40:12 +0000

    put in the correct script file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var songs = ["No Guidance","Que tal si eres tu"," No idea","como olvidarte"];
 var Artist = ["Chris Brown","Los tigeres del norte","Don Toliver","El Bebeto"];
  songs.forEach(function(song) {
-    $(".songName").text(song); 
-      // $(".songs").append("<div>" + song + "</div>");
+    
+      $(".songName").append("<div>" + song + "</div>");
     }); 
 //     Artist.forEach(function(song) {
 //       $(".Artist").append("<div>" + song + "</div>");
