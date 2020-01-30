5621f5fc43bf871e13a7a2692bb40f5c6fb74951
jessicaespinoza1 <13jessica.espinoza@gmail.com>
2020-01-30 02:31:27 +0000

    worked on css and javascript

--- js/script.js
+++ js/script.js
@@ -1,34 +1,55 @@
-// var songs = ["No Guidance","Que tal si eres tu"," No idea","como olvidarte"];
-// var Artist = ["Chris Brown","Los tigeres del norte","Don Toliver","El Bebeto"];
-//  songs.forEach(function(song) {
-    
-//       $(".songName").append("<div>" + song + "</div>");
-//     }); 
-// //     Artist.forEach(function(song) {
-// //       $(".Artist").append("<div>" + song + "</div>");
-// //     }); 
-let songOne ={
+ //var songs = ["No Guidance","Que tal si eres tu"," No idea","como olvidarte"];
+ //var Artist = ["Chris Brown","Los tigeres del norte","Don Toliver","El Bebeto"];
+//songs.forEach(function(song) {
+  
+     // $(".songName").append("<div>" + song + "</div>");
+ //}); 
+  //  Artist.forEach(function(song) {
+   //  $(".Artist").append("<div>" + song + "</div>");
+  // }); 
+
+
+    let songOne ={
   artist: "Chris Brown",
   song: "No Guidance",
   time: "3:20",
   image:"https://i.ytimg.com/vi/omG0feZyv7Q/maxresdefault.jpg",
   link:"https://www.youtube.com/watch?v=oOni4BMeMp0"
+  
 }
-let songTwo ={
+    let songTwo ={
   artist: "Los tigeres del norte",
   song: "Que tal si eres tu",
   time: "2:30",
   image:"https://ewscripps.brightspotcdn.com/dims4/default/f8d5305/2147483647/strip/true/crop/512x288+64+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fmediaassets.ktnv.com%2Fphoto%2F2016%2F08%2F19%2Flostigres_1471641388178_44641586_ver1.0_640_480.jpg",
+  link:""
 }
-let songThree ={
+    let songThree ={
   artist: "Don Toliver",
   song: "No idea",
   time: "2:10",
   image:"https://i1.sndcdn.com/artworks-000530933148-z7sxkr-t500x500.jpg",
+  link:""
 }
-let songFour ={
+    let songFour ={
   artist: "El Bebeto",
   song: " Como olividarte",
   time: "3:15",
   image:"https://m.media-amazon.com/images/I/81RFcFDUPQL._SS500_.jpg",
+  link:""
 }
+var objects = [songOne,songTwo,songThree,songFour];
+objects.forEach(function(object) {
+
+  $(".songobjects").append("<div>" + object.song + "</div>");
+  $(".artistobjects").append("<div>" + object.artist + "</div>")
+  $(".timeobjects").append("<div>" + object.time + "</div>")
+  $(".imageobjects").append("<div>" + object.image + "</div>")
+  $(".linkobjects").append("<div>" + object.link + "</div>")
+
+
+
+
+});
+
+
